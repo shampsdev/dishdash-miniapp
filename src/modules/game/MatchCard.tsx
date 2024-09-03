b463bfacb30a1552d647cb76bb3ec65813b1550d
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-09-03 23:15:02 +0200

    fixed weird image ratio

--- src/modules/game/MatchCard.tsx
+++ src/modules/game/MatchCard.tsx
@@ -4,8 +4,6 @@ import SwipeTag from './swipe-tags';
 import { useSwipes } from '@/shared/providers/swipe.provider';
 import { useMatchStore } from '@/shared/stores/match.store';
 
-const categories = ['Кофе', 'Развлечения', 'Чай', 'Новые ощущения'];
-
 const MatchCard = () => {
   const { card } = useMatchStore();
   const { vote } = useSwipes();
@@ -23,10 +21,15 @@ const MatchCard = () => {
         className="w-full aspect-[100/170] max-w-[320px] xs:max-w-[420px] relative z-10"
       >
         <div className="h-[360px] w-full xs:h-[420px] relative">
-          <img className="rounded-3xl" src={card?.image} />
+          <div className="bg-slate-100 h-full w-full rounded-t-3xl overflow-hidden">
+            <img
+              className="h-full w-auto min-w-full object-cover"
+              src={card?.image}
+            />
+          </div>
           <div className="absolute w-[90%] top-4 left-0 right-0 mx-auto flex justify-between items-center">
             <h3 className="py-2 px-4 rounded-3xl bg-white bg-opacity-80 backdrop-blur-sm">
-              {card?.title}
+              {card?.title.split(', ')[0]}
             </h3>
             <ButtonIcon
               variant="outline"
@@ -38,8 +41,8 @@ const MatchCard = () => {
         </div>
         <div className="-translate-y-12 pt-4 h-52 w-full rounded-3xl bg-white shadow-md overflow-hidden">
           <div className="mx-4 flex flex-wrap gap-2">
-            {categories.map((el, index) => (
-              <SwipeTag key={index}>{el}</SwipeTag>
+            {card?.tags.map((el, index) => (
+              <SwipeTag key={index}>{el.name}</SwipeTag>
             ))}
           </div>
           <p className="p-4">{card?.description}</p>
--- src/pages/result.page.tsx
+++ src/pages/result.page.tsx
@@ -3,8 +3,6 @@ import { ButtonIcon } from '@/components/ui/button-icon';
 import SwipeTag from '@/modules/game/swipe-tags';
 import { useResultCardStore } from '@/shared/stores/result-card.store';
 
-const categories = ['Кофе', 'Развлечения', 'Чай', 'Новые ощущения'];
-
 const ResultPage = () => {
   const { card } = useResultCardStore();
 
@@ -18,10 +16,15 @@ const ResultPage = () => {
         className="w-full aspect-[100/170] max-w-[320px] xs:max-w-[420px] relative z-10"
       >
         <div className="h-[360px] w-full xs:h-[420px] relative">
-          <img className="rounded-3xl" src={card?.image} />
+          <div className="bg-slate-100 h-full w-full rounded-t-3xl overflow-hidden">
+            <img
+              className="h-full w-auto min-w-full object-cover"
+              src={card?.image}
+            />
+          </div>
           <div className="absolute w-[90%] top-4 left-0 right-0 mx-auto flex justify-between items-center">
             <h3 className="py-2 px-4 rounded-3xl bg-white bg-opacity-80 backdrop-blur-sm">
-              {card?.title}
+              {card?.title.split(', ')[0]}
             </h3>
             <ButtonIcon
               variant="outline"
@@ -33,8 +36,8 @@ const ResultPage = () => {
         </div>
         <div className="-translate-y-12 pt-4 h-52 w-full rounded-3xl bg-white shadow-md overflow-hidden">
           <div className="mx-4 flex flex-wrap gap-2">
-            {categories.map((el, index) => (
-              <SwipeTag key={index}>{el}</SwipeTag>
+            {card?.tags.map((el, index) => (
+              <SwipeTag key={index}>{el.name}</SwipeTag>
             ))}
           </div>
           <p className="p-4">{card?.description}</p>
