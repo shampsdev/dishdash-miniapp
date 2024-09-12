fe5d0864eaec26507ebc15c510e8e672fe5c04d8
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-09-12 20:15:22 +0300

    Removed hardcode, added await

--- src/shared/api/tags.api.ts
+++ src/shared/api/tags.api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { Tag } from '../types/tag.type';
+import { API_URL } from '../constants';
 
 export const fetchTags = async (): Promise<Tag[] | undefined> => {
-  return axios
-    .get<Tag[]>(`https://dishdash.ru/api/v1/places/tags`)
-    .then((res) => res.data)
-    .catch((err) => {
-      console.error(err);
-      return undefined;
-    });
+  try {
+    const response = await axios.get<Tag[]>(`${API_URL}/api/v1/places/tags`);
+    return response.data;
+  } catch (err) {
+    console.error('Error fetching tags:', err);
+    return undefined;
+  }
 };
