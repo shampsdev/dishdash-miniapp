1bbca914a026871c5263f7c066cef505a5ea1d37
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-09-12 20:19:03 +0300

    Refactored match store

--- src/shared/stores/match.store.ts
+++ src/shared/stores/match.store.ts
@@ -4,14 +4,19 @@ import { create } from 'zustand';
 interface MatchStore {
   id: number | null;
   card: Card | null;
-  setMatchCard: (card: Card | null, id: number | null) => void;
 }
 
-export const useMatchStore = create<MatchStore>()((set) => ({
-  id: null,
-  card: null,
-  setMatchCard: (card: Card | null) => set({ card }),
-}));
+interface MatchStoreActions {
+  setMatchCard: (card: MatchStore) => void;
+}
+
+export const useMatchStore = create<MatchStore & MatchStoreActions>()(
+  (set) => ({
+    id: null,
+    card: null,
+    setMatchCard: (card: MatchStore) => set(card),
+  }),
+);
 
 export function getMatchStoreMethods() {
   const { id, card, setMatchCard } = useMatchStore.getState();
