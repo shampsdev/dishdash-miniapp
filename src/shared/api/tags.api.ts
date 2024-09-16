ae127111e671efe216715a3341289377ef5f6f2b
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-09-16 12:37:51 +0300

    Ooops

--- src/shared/api/tags.api.ts
+++ src/shared/api/tags.api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Tag } from '../types/tag.type';
+import { Tag } from '../types/tag.interface';
 import { API_URL } from '../constants';
 
 export const fetchTags = async (): Promise<Tag[] | undefined> => {
