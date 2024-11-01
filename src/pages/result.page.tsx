36af0539d88b379dc4affe83407df9d4b489292c
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-11-01 18:27:51 +0300

    feat(end-screen): Updated end screen. (#43)
    * Updated dockerfile to match new standard
    
    * added cd for dev branch (#22)
    
    Co-authored-by: Mike de Geofroy <mikedegeofroy@gmail.com>
    
    * fixed formatting
    
    * Moved api url to env
    
    Cleaned up useless console log
    
    * Added .env info log
    
    * fix(env): pushed env lol
    
    * Added gitignore
    
    * Added .env values
    
    * Updated cd pipelines. Added envs into build
    
    * added copy .env
    
    * Removed copy dev :)
    
    * Update cd-dev.yaml
    
    * feat(settings): Updated Settings Page (#32)
    
    * Added avatars to settings page
    
    * Updated settings page
    
    * Remove envs
    
    * Comment sandwitch
    
    * remove secret
    
    * blank
    
    * fix(lint): Fixed bad lint problem
    
    * feat(cards): Updated the card design (#34)
    
    * Added basic card components.
    
    * adding opacity on like & dislikes
    
    * Added likes and dislikes to cards
    
    * fix(android): Updated Cards
    
    * feat(end-screen): Added an end screen. (#42)
    
    * Updated result interface and added basic markup for result page.
    
    * Added card bottom icons.
    
    * Added time to cards
    
    * fix(lint): fixed lint errors
    
    * fixed settings error
    
    * Removed unused assets
    
    ---------
    
    Co-authored-by: Alex Dyakonov <115474788+AlexDyakonov@users.noreply.github.com>
    Co-authored-by: Alex Dyakonov <shram.monolit@mail.ru>
    Co-authored-by: vaniog <vaniog@yandex-team.ru>

--- src/pages/result.page.tsx
+++ src/pages/result.page.tsx
@@ -1,26 +1,21 @@
-import { CardComponent } from '@/modules/game/card';
+import { ResultCard } from '@/modules/game/result.card';
 import { useResultCardStore } from '@/shared/stores/result-card.store';
-import { useWebApp } from '@vkruglikov/react-telegram-web-app';
 
 const ResultPage = () => {
-  const { card } = useResultCardStore();
-  const { close } = useWebApp();
+    const { result } = useResultCardStore();
 
-  close();
+    console.log(result)
 
-  return (
-    <div
-      className="flex min-h-screen h-full flex-col justify-center items-center overflow-hidden  ${
-      isDragging"
-    >
-      <div
-        id="cardsWrapper"
-        className="w-full aspect-[100/170] max-w-[320px] xs:max-w-[420px] relative z-10"
-      >
-        {card && <CardComponent data={card} />}
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className="min-h-screen h-full overflow-hidden p-5"
+        >
+            <h1 className="text-xl font-medium" >Ваши метчи</h1>
+            <div className="space-y-5 h-full w-full pt-4">
+                {result?.matches.map((x) => <ResultCard key={`${x.id}_${x.card.id}`} card={x.card} />)}
+            </div>
+        </div>
+    );
 };
 
 export default ResultPage;
