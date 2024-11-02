1c3e2d84128460cbf6f66f2b1dda77241aaf2407
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-11-02 19:23:51 +0300

    Added a map on home screen.

--- src/shared/api/lobby.api.ts
+++ src/shared/api/lobby.api.ts
@@ -3,26 +3,28 @@ import { API_URL } from '../constants';
 import { Location } from '../types/location.interface';
 
 interface Lobby {
-  id: string;
-  location: Location;
+    id: string;
+    location: Location;
 }
 
 export const fetchLobby = async (id: string): Promise<Lobby | undefined> => {
-  try {
-    const response = await axios.get<Lobby>(`${API_URL}/api/v1/lobbies/${id}`);
-    return response.data;
-  } catch (err) {
-    console.error('Error fetching tags:', err);
-    return undefined;
-  }
+    try {
+        const response = await axios.get<Lobby>(`${API_URL}/api/v1/lobbies/${id}`);
+        return response.data;
+    } catch (err) {
+        console.error('Error fetching tags:', err);
+        return undefined;
+    }
 };
 
-export const postLobby = async (id: string): Promise<Lobby | undefined> => {
-  try {
-    const response = await axios.get<Lobby>(`${API_URL}/api/v1/lobbies/${id}`);
-    return response.data;
-  } catch (err) {
-    console.error('Error fetching tags:', err);
-    return undefined;
-  }
+export const postLobby = async (location: Location): Promise<Lobby | undefined> => {
+    try {
+        const response = await axios.post<Lobby>(`${API_URL}/api/v1/lobbies`, {
+            location
+        });
+        return response.data;
+    } catch (err) {
+        console.error('Error fetching tags:', err);
+        return undefined;
+    }
 };
