faeee4fa3db7ba319099607d6887aa37abb6bfd2
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-11-25 03:26:22 +0300

    Removed Banner
    Revert "added raw version of home page banner"
    
    This reverts commit 0c47dd59af643e82fcb66ff02feb6d3b2404caaa.
    
    Revert "added banner to home page"
    
    This reverts commit 9712feeaf0d9a6555e830afff4372e7545f71f32.

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -8,11 +8,10 @@ import { LobbyCard } from "@/modules/home/lobby.card";
 import { MapButton } from "@/modules/home/map.button";
 import { Avatar } from "@/components/ui/avatar";
 
-import hat from "@/assets/hat.png";
 
 export const HomePage = () => {
     const webApp = useWebApp();
-    const { enableVerticalSwipes, disableVerticalSwipes, openTelegramLink } = webApp;
+    const { enableVerticalSwipes, disableVerticalSwipes } = webApp;
     const { user, recentLobbies, logoutUser } = useAuth();
     const [isExpanded, expand] = useExpand();
 
@@ -28,49 +27,41 @@ export const HomePage = () => {
     }, [isExpanded]);
 
     return (
-        <div className="flex flex-col spacy-y-5 overflow-y-hidden h-svh mt-5">
+        <div className="flex flex-col overflow-y-hidden h-svh mt-5">
             <AnimatePresence>
                 {!open && (
                     <motion.div
-                        initial={{ opacity: 0, height: '0vh' }}
-                        animate={{ opacity: 1, height: '40vh' }}
+                        initial={{ opacity: 0, height: '0px' }}
+                        animate={{ opacity: 1, height: 'auto' }}
                         exit={{ opacity: 0, height: '0px' }}
-                        className="flex space-y-5 flex-col mx-5 mb-5"
-                        transition={{ duration: 0.3, ease: [0.25, 0.8, 0.5, 1] }}
+                        className="pb-auto space-y-5"
                         onClick={() => {
                             logoutUser();
                             expand();
                         }}
                     >
-                        <div className="flex gap-5 items-center">
-                            {user && (
-                                <Avatar
-                                    src={user.avatar}
-                                    fallback="?"
-                                    style={{ maxHeight: '90px', maxWidth: '90px', borderWidth: '5px' }}
-                                    fallbackElement={
-                                        <span className="text-[50px] font-medium text-primary">
-                                            {user?.name.split(' ').slice(0, 2).map(x => x.charAt(0)).join('').toUpperCase()}
-                                        </span>
-                                    }
-                                />
-                            )}
-                            <h1 className="text-2xl font-medium">Привет, <br /> {user?.name}! </h1>
-                        </div>
-                        <div onTouchStart={() => {
-                            openTelegramLink('https://t.me/shampsdev');
-                        }} className="h-full w-full bg-secondary overflow-hidden rounded-xl">
-                            <img src={hat} className="h-full w-full object-cover" />
-                        </div>
+                        {user && (
+                            <Avatar
+                                src={user.avatar}
+                                fallback="?"
+                                style={{ maxHeight: '100px', maxWidth: '100px', borderWidth: '5px', margin: 'auto' }}
+                                fallbackElement={
+                                    <span className="text-[50px] font-medium text-primary">
+                                        {user?.name.split(' ').slice(0, 2).map(x => x.charAt(0)).join('').toUpperCase()}
+                                    </span>
+                                }
+                            />
+                        )}
+                        <h1 className="text-2xl font-medium text-center">Привет, <br /> {user?.name}! </h1>
                     </motion.div>
                 )}
             </AnimatePresence>
-            <div className="flex flex-col justify-end w-[90%] mx-auto mb-5">
+            <div className="h-full flex flex-col justify-end w-[90%] mx-auto mb-5">
                 <motion.div
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ duration: 0.1, ease: [0.25, 0.8, 0.5, 1] }}
-                    className="w-full gap-y-5 flex flex-col justify-end mb-5"
+                    className="w-full gap-y-5 flex flex-col justify-end mt-auto mb-auto"
                 >
                     <h1 className="text-center font-medium text-2xl">Последние лобби</h1>
                     {recentLobbies.length > 0 ?
