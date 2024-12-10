eb2f65f10ae8e029979afe2019d394c213bb1784
priest_faria <timurchikus@gmail.com>
2024-12-10 17:59:58 +0300

    refactor: add color filter component and decision for card swipe (like and dislike)

--- src/modules/game/card.tsx
+++ src/modules/game/card.tsx
@@ -8,6 +8,8 @@ import { getTime } from '@/shared/util/time.util';
 import HeartIcon from '@/assets/icons/heart.icon.tsx';
 import WalkIcon from '@/assets/icons/walk.icon';
 import CrossIcon from '@/assets/icons/cross.icon.tsx';
+import ColorFilter from '@/components/colorFilter';
+import CardDecision from '@/components/cardDecision';
 
 interface Props {
   data: Card;
@@ -17,6 +19,7 @@ interface Props {
 export const CardComponent = ({ data, deltaY }: Props) => {
   const [expanded, setExpanded] = useState(false);
   const { settings } = useLobbyStore();
+  const [imageIndex, setImageIndex] = useState(0);
 
   const { openLink } = useWebApp();
 
@@ -34,6 +37,30 @@ export const CardComponent = ({ data, deltaY }: Props) => {
   const iconLeftOpacity = deltaY
     ? useTransform(deltaY, [-5, 0, 5], [0, 0, 1])
     : 0;
+
+  const cardImageOnTapHandler = (e: MouseEvent) => {
+    const boundingBox = (e.target as HTMLElement).getBoundingClientRect();
+    const tapX = e.clientX - boundingBox.left;
+    const elementWidth = boundingBox.width;
+
+    setImageIndex((prevIndex) => {
+      if (tapX + elementWidth / 4 < elementWidth / 2) {
+        return (prevIndex - 1 + data.images.length) % data.images.length;
+      } else if (tapX - elementWidth / 4 > elementWidth / 2) {
+        return (prevIndex + 1) % data.images.length;
+      }
+      return prevIndex;
+    });
+  };
+
+  const followPlaceURL = () => {
+    const url =
+      data.url !== null && data.url !== ''
+        ? data.url
+        : `https://yandex.ru/maps/?rtext=${data.location.lat}%2C${data.location.lon}`;
+    openLink(url);
+  };
+
   const { disableVerticalSwipes, enableVerticalSwipes } = useWebApp();
   useEffect(() => {
     disableVerticalSwipes();
@@ -60,18 +87,10 @@ export const CardComponent = ({ data, deltaY }: Props) => {
     }
   };
 
-  const [imageIndex, setImageIndex] = useState(0);
-
   return (
     <div className="relative h-full overflow-hidden rounded-3xl">
-      <motion.div
-        style={{ opacity: rightOpacity }}
-        className="pointer-events-none absolute inset-0 bg-secondary z-10"
-      ></motion.div>
-      <motion.div
-        style={{ opacity: leftOpacity }}
-        className="pointer-events-none absolute inset-0 bg-primary z-10"
-      ></motion.div>
+      <ColorFilter opacity={rightOpacity} colorStyle="bg-secondary" />
+      <ColorFilter opacity={leftOpacity} colorStyle="bg-primary" />
       <div className="w-full aspect-square">
         <div className="bg-slate-100 h-full w-full rounded-t-3xl pb-4 overflow-hidden">
           <img
@@ -93,39 +112,13 @@ export const CardComponent = ({ data, deltaY }: Props) => {
           })}
       </div>
       {deltaY && (
-        <div className="absolute w-full top-1/3 flex justify-center h-14 -translate-y-1/3 z-20">
-          <motion.div style={{ opacity: iconLeftOpacity }} className="absolute">
-            <HeartIcon
-              className="bg-white rounded-full p-3"
-              fill="fill-primary"
-            />
-          </motion.div>
-          <motion.div
-            style={{ opacity: iconRightOpacity }}
-            className="absolute"
-          >
-            <CrossIcon
-              className="bg-primary rounded-full p-4"
-              fill="fill-primary-foreground"
-            />
-          </motion.div>
-        </div>
+        <CardDecision
+          rightOpacity={iconRightOpacity}
+          leftOpacity={iconLeftOpacity}
+        />
       )}
       <motion.div
-        onTap={(e: MouseEvent) => {
-          const boundingBox = (e.target as HTMLElement).getBoundingClientRect();
-          const tapX = e.clientX - boundingBox.left;
-          const elementWidth = boundingBox.width;
-
-          setImageIndex((prevIndex) => {
-            if (tapX + elementWidth / 4 < elementWidth / 2) {
-              return (prevIndex - 1 + data.images.length) % data.images.length;
-            } else if (tapX - elementWidth / 4 > elementWidth / 2) {
-              return (prevIndex + 1) % data.images.length;
-            }
-            return prevIndex;
-          });
-        }}
+        onTap={cardImageOnTapHandler}
         className="absolute top-0 w-full h-full"
       >
         <motion.div
@@ -160,16 +153,7 @@ export const CardComponent = ({ data, deltaY }: Props) => {
                 {data?.description}
               </div>
               {expanded && (
-                <div
-                  onClick={() => {
-                    const url =
-                      data.url !== null && data.url !== ''
-                        ? data.url
-                        : `https://yandex.ru/maps/?rtext=${data.location.lat}%2C${data.location.lon}`;
-                    openLink(url);
-                  }}
-                  className="underline pt-2"
-                >
+                <div onClick={followPlaceURL} className="underline pt-2">
                   {data.address}
                 </div>
               )}
