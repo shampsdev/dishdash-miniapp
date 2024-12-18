d59492c480b3249a2e4dbedc5e48889029410703
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-12-19 01:35:32 +0300

    Added a new home page (#87)

---
+++ src/components/carouselCard.tsx
@@ -0,0 +1,32 @@
+interface CarouselCardProps {
+  src: string;
+  onClick: () => void;
+}
+
+export const CarouselCard = ({ src, onClick }: CarouselCardProps) => {
+  return (
+    <div className="w-[90%] mx-auto select-none overflow-hidden rounded-xl">
+      <div className="aspect-[7/4] bg-white">
+        <img
+          src={src}
+          alt=""
+          className="h-full w-full pointer-events-none object-cover"
+        />
+      </div>
+      <div className="aspect-[7/2] bg-secondary flex items-center p-4">
+        <div>
+          <h1 className="font-semibold">
+            Telegram-канал команды разработчиков
+          </h1>
+          <p>Все о нас и наших проектах</p>
+        </div>
+        <div
+          onClick={onClick}
+          className="bg-primary active:opacity-90 cursor-pointer h-fit py-2 px-4 rounded-3xl"
+        >
+          <p className="font-medium">Перейти</p>
+        </div>
+      </div>
+    </div>
+  );
+};
