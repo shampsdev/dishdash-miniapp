c2c2485fc5e7b9370448cfbf7f70c08e1748dcc5
priest_faria <timurchikus@gmail.com>
2024-12-19 08:51:31 +0300

    fixed fonts

--- src/modules/settings/tags.tsx
+++ src/modules/settings/tags.tsx
@@ -1,11 +1,9 @@
 import { settingsUpdateEvent } from '@/shared/events/app-events/settings.event';
 import { useLobbyStore } from '@/shared/stores/lobby.store';
 import { Toggle } from '@/components/ui/toggle';
-
 export const Tags = () => {
   const { settings, tags } = useLobbyStore();
-  // tags.push(...tags);
-  // tags.push(...tags);
+
   const toggleCategoryType = (tagId: number) => {
     const found = settings.tags.find((x) => x == tagId);
     let updatedTags: number[] = [];
@@ -28,19 +26,18 @@ export const Tags = () => {
   return (
     <>
       {tags
-        .sort((a, b) => a.id - b.id)
+        .sort((a, b) => a.order - b.order)
         .map((tag) => (
           <Toggle
             key={tag.id}
             pressed={settings.tags.some((x) => x === tag.id)}
             className={
               'flex flex-col items-center rounded-xl transition-colors bg-secondary border-none duration-150 w-full h-fit'
-              // 'flex flex-row gap-5 items-center justify-start py-8 px-3 rounded-xl transition-colors bg-secondary border-none duration-150 w-full'
             }
             onClick={() => toggleCategoryType(tag.id)}
           >
-            <div className="h-[90%] aspect-square  rounded-lg">
-              <img className="aspect-square" src={tag.icon} />
+            <div className="h-[70%] w-[60%] aspect-square  rounded-lg flex justify-center items-center">
+              <img className="w-full h-full aspect-square" src={tag.icon} />
             </div>
             <div className="mb-4">
               <span className="text-lg font-normal">{tag.name}</span>
