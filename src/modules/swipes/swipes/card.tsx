eacdaf2be741ae44ef56e5e4f678d63adbccffcc
Mike de Geofroy <mikedegeofroy@gmail.com>
2025-03-09 14:54:44 +0300

    trying to fix bad rendering on result screen

--- src/modules/swipes/swipes/card.tsx
+++ src/modules/swipes/swipes/card.tsx
@@ -160,7 +160,7 @@ export const CardComponent = ({ data, deltaY, last }: CardComponentProps) => {
           {data.card.tags.map((el) => el.name).join(', ')}
         </p>
         <div className="w-full grid grid-cols-2 gap-4 px-4 pt-3">
-          <div className="bg-secondary-foreground font-medium text-center py-1 rounded-xl">
+          <div className="bg-secondary-foreground font-medium text-center py-1 rounded-xl line-clamp-1">
             ~ {data.card.priceAvg} ₽
           </div>
           <div className="flex bg-secondary-foreground font-medium gap-1 justify-center items-center py-1 rounded-xl">
