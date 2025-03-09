c6d335e03205b7f19cd43458bcb746bfbd793ed1
Mike de Geofroy <mikedegeofroy@gmail.com>
2025-03-09 06:34:40 +0300

    Started adding UI movement

---
+++ src/pages/swipes/result/result.page.tsx
@@ -0,0 +1,98 @@
+import { Icons } from '@/assets/icons/icons';
+import { ResultCard } from '@/modules/swipes/results/result-card';
+import { useResultStore } from '@/modules/swipes/results/result.store';
+import useTheme from '@/shared/hooks/useTheme';
+import { useServerRouteStore } from '@/shared/stores/server-route.store';
+import { MainButton, useWebApp } from '@vkruglikov/react-telegram-web-app';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1
+    }
+  }
+};
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      ease: 'easeOut'
+    }
+  }
+};
+
+export const ResultPage = () => {
+  const { result } = useResultStore();
+  const webApp = useWebApp();
+  const navigate = useNavigate();
+
+  const { background } = useTheme();
+
+  const { setRoute } = useServerRouteStore();
+
+  const setMainScreen = () => {
+    navigate('/');
+  };
+
+  const setSwipes = () => {
+    setRoute('swiping');
+  };
+
+  useEffect(() => {
+    webApp.BackButton.show();
+    webApp.BackButton.onClick(setSwipes);
+
+    return () => {
+      webApp.BackButton.hide();
+      webApp.BackButton.offClick(setSwipes);
+    };
+  }, []);
+
+  return (
+    <div className="min-h-screen h-full p-5">
+      <div className="flex pb-2 justify-between">
+        <h1 className="text-xl font-medium pb-2">Ваши мэтчи</h1>
+      </div>
+      <motion.div
+        onTap={setSwipes}
+        className="active:scale-75 transition-all absolute top-[5vw] right-[5vw]"
+      >
+        <Icons.swipes fill={background} className="text-primary" />
+      </motion.div>
+      <AnimatePresence>
+        <motion.div
+          className="space-y-5 h-screen overflow-scroll w-full pt-4 pb-20"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          layout
+        >
+          {result?.top.map((x) => (
+            <motion.div
+              key={`result_card_${x.card.id}`}
+              variants={childVariants}
+            >
+              <ResultCard card={x.card} likes={x.likes} />
+            </motion.div>
+          ))}
+        </motion.div>
+      </AnimatePresence>
+
+      <MainButton onClick={setMainScreen} text={'На Главную'} />
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss';
-import tailwindcssAnimate from 'tailwindcss-animate';
 
 const config: Config = {
   darkMode: ['class'],
@@ -7,7 +6,7 @@ const config: Config = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}'
   ],
   prefix: '',
   theme: {
@@ -15,32 +14,32 @@ const config: Config = {
       center: true,
       padding: '2rem',
       screens: {
-        '2xl': '1400px',
-      },
+        '2xl': '1400px'
+      }
     },
     extend: {
       colors: {
         answerBad: {
-          500: '#F75064',
+          500: '#F75064'
         },
         answerGood: {
-          500: '#82D350',
+          500: '#82D350'
         },
         grey: {
-          500: '#6A6A6A',
+          500: '#6A6A6A'
         },
         blue: {
-          500: '#051B71',
+          500: '#051B71'
         },
         green: {
           300: '#D3E0BE',
           800: '#5c7537',
-          900: '#34441c',
+          900: '#34441c'
         },
         gameSwipe: {
           left: '#fcbab6',
           neutral: '#fafafa',
-          right: '#D4E0B2',
+          right: '#D4E0B2'
         },
 
         border: 'var(--border)',
@@ -50,55 +49,64 @@ const config: Config = {
         foreground: 'var(--foreground)',
         primary: {
           DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)',
+          foreground: 'var(--primary-foreground)'
         },
         secondary: {
           DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)',
+          foreground: 'var(--secondary-foreground)'
         },
         destructive: {
           DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)',
+          foreground: 'var(--destructive-foreground)'
         },
         muted: {
           DEFAULT: 'var(--muted)',
-          foreground: 'var(--muted-foreground)',
+          foreground: 'var(--muted-foreground)'
         },
         accent: {
           DEFAULT: 'var(--accent)',
-          foreground: 'var(--accent-foreground)',
+          foreground: 'var(--accent-foreground)'
         },
         popover: {
           DEFAULT: 'var(--popover)',
-          foreground: 'var(--popover-foreground)',
+          foreground: 'var(--popover-foreground)'
         },
         card: {
           DEFAULT: 'var(--card)',
-          foreground: 'var(--card-foreground)',
-        },
+          foreground: 'var(--card-foreground)'
+        }
       },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        sm: 'calc(var(--radius) - 4px)'
       },
       keyframes: {
         'accordion-down': {
           from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 'var(--radix-accordion-content-height)' }
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+          to: { height: '0' }
         },
+        scale: {
+          '0%, 100%': {
+            transform: 'scale(1.05)'
+          },
+          '50%': {
+            transform: 'scale(0.95)'
+          }
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-      },
-    },
+        scale: 'scale 0.5s linear infinite'
+      }
+    }
   },
-  plugins: [tailwindcssAnimate],
+  plugins: []
 };
 
 export default config;
